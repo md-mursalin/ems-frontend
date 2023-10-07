9e7459a0cfd91dd8d830a01cae525c2fb3520690
Mursalin <[mursalinxz1@gmail.com]>
2023-10-07 22:55:04 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import Home from "./Home";
+import EmployeeView from "./components/employee/EmployeeView";
+import NavBar from "./components/common/NavBar";
+import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import AddEmployee from "./components/employee/AddEmployee";
+import EditEmployee from "./components/employee/EditEmployee";
+import EmployeeProfile from "./components/employee/EmployeeProfile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="container mt-5">
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={<Home />}></Route>
+                    <Route
+                        exact
+                        path="/view-employees"
+                        element={<EmployeeView />}></Route>
+                    <Route
+                        exact
+                        path="/add-employees"
+                        element={<AddEmployee />}></Route>
+                    <Route
+                        exact
+                        path="/edit-employee/:id"
+                        element={<EditEmployee />}></Route>
+                    <Route
+                        exact
+                        path="/employee-profile/:id"
+                        element={<EmployeeProfile />}></Route>
+                </Routes>
+            </Router>
+        </main>
+    );
 }
 
 export default App;
